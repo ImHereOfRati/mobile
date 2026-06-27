a1023b7c100bb5514ff8ff3634fab55db3dcf30b
Ko Dong Su <127917642+kdongsu5509@users.noreply.github.com>
2026-06-27 20:58:55 +0900

    feat(fcm): add Kakao-style foreground banner (#65)
    * feat(setting): add account withdrawal flow
    
    * feat(fcm): add Kakao-style foreground banner

---
+++ test/integration/fcm/fcm_notification_policy_test.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:iamhere/integration/fcm/fcm_notification_policy.dart';
+
+void main() {
+  group('resolveFcmChannelId', () {
+    test('도착/출발 알림은 critical 채널을 사용한다', () {
+      expect(resolveFcmChannelId('ARRIVAL'), criticalChannelId);
+      expect(resolveFcmChannelId('ARRIVAL_CONFIRMATION'), criticalChannelId);
+      expect(resolveFcmChannelId('DEPARTURE'), criticalChannelId);
+    });
+
+    test('친구 요청/위치 공유는 high 채널을 사용한다', () {
+      expect(resolveFcmChannelId('FRIEND_REQUEST_RECEIVED'), highChannelId);
+      expect(resolveFcmChannelId('LOCATION_SHARE_RECEIVED'), highChannelId);
+    });
+
+    test('친구 수락/발송 실패는 normal 채널을 사용한다', () {
+      expect(resolveFcmChannelId('FRIEND_REQUEST_ACCEPTED'), normalChannelId);
+      expect(resolveFcmChannelId('DELIVERY_FAILED_NOTICE'), normalChannelId);
+    });
+
+    test('공지/발송 결과는 silent 채널을 사용한다', () {
+      expect(resolveFcmChannelId('TERMS_UPDATE_NOTICE'), silentChannelId);
+      expect(resolveFcmChannelId('DELIVERY_RESULT_NOTICE'), silentChannelId);
+    });
+
+    test('알 수 없는 타입도 silent 채널로 떨어진다', () {
+      expect(resolveFcmChannelId('UNKNOWN_TYPE'), silentChannelId);
+      expect(resolveFcmChannelId(null), silentChannelId);
+    });
+  });
+}
